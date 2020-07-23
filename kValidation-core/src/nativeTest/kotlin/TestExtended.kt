c2a0fa452c290235e9c58237e43e4576ab0aa087
Julian Hahn <julian@hahn-berlin.de>
2020-07-23 21:29:50 +0200

    added test for each platform to showcase the problem

---
+++ kValidation-core/src/nativeTest/kotlin/TestExtended.kt
@@ -0,0 +1,32 @@
+import de.kotlinBerlin.kValidation.Invalid
+import de.kotlinBerlin.kValidation.Validation
+import de.kotlinBerlin.kValidation.constraints.minLength
+import kotlin.test.Test
+
+class TestExtended {
+
+    data class Person(val name: String?, val familyName: String?, val parent: Person?)
+
+    @Test
+    fun test1() {
+        val singleValidation = Validation<Person> {
+            Person::name required {
+                minLength(1).asWarning()
+            }
+            Person::name required {
+                minLength(2)
+            }
+        }
+
+        val validation = Validation.and<Person> {
+            Person::parent ifPresent {
+                run(singleValidation)
+            }
+            run(singleValidation)
+        }
+        val validate = validation.invoke(Person("", null, Person("", null, null)))
+        if (validate is Invalid<*>) {
+            println(validate.print())
+        }
+    }
+}
